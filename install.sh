39ce9bdb0608de43d2b7f78a95c462bd8c62871c
NOC-OLETV <github@oletv.net.br>
2025-09-18 10:02:36 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 apt install zabbix-agent
 sed -i 's/Server=127.0.0.1/Server=200.188.244.242/' /etc/zabbix/zabbix_agentd.conf
 sed -i 's/# ListenPort=10050/ListenPort=10050/' /etc/zabbix/zabbix_agentd.conf
+chmod 777 /usr/local/src/coletando.sh
+chmod 777 /usr/local/src/gera_oid.txt
